42dfbb26c0d862f314329ac3d27076f48b9c40b0
Saloni403 <156641454+Saloni403@users.noreply.github.com>
2025-11-07 13:19:10 +0530

    home, about, blog, services, frenchise

--- web/js/script.js
+++ web/js/script.js
@@ -4,7 +4,7 @@ function openMenu(){
 
 window.addEventListener("scroll", function() {
   const header = document.querySelector("header");
-  if (window.scrollY > 180) {
+  if (window.scrollY > 100) {
     header.classList.add("sticky");
   } else {
     header.classList.remove("sticky");
