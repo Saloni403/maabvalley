a3ac3f054d87102c03d2b3dce5366e8342bc3ec5
Saloni403 <156641454+Saloni403@users.noreply.github.com>
2025-11-19 16:28:41 +0530

    changes

--- web/js/servicedesc.js
+++ web/js/servicedesc.js
@@ -1,4 +1,5 @@
 document.querySelectorAll(".servicecard").forEach(service => {
+    service.style.cursor="pointer"
 service.addEventListener("click",()=>{
     document.querySelector(".servicedesc").style.display="flex"
 })
